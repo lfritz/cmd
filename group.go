ccc845f8d5974c9766517d9aabc4288eb6ce990e
Levin Fritz <levin.fritz@gmail.com>
2020-06-13 08:49:55 +0200

    Implement Group.Run method

--- group.go
+++ group.go
@@ -1,9 +1,17 @@
 package cmd
 
+import (
+	"fmt"
+	"os"
+)
+
 // A Group represents a group of commands. Groups can be nested arbitrarily.
 type Group struct {
 	Flags
 	Summary, Details string
+	name             string
+	groups           map[string]*Group
+	commands         map[string]*Cmd
 }
 
 // NewGroup returns a new group of commands with the specified name.
@@ -13,12 +21,16 @@ func NewGroup(name string) *Group {
 
 // Command adds a command. The given function will be called if this command is selected.
 func (g *Group) Command(name string, f func()) *Cmd {
-	return nil
+	command := New(name)
+	g.commands[name] = command
+	return command
 }
 
 // Group adds a sub-group.
 func (g *Group) Group(name string) *Group {
-	return nil
+	group := NewGroup(name)
+	g.groups[name] = group
+	return group
 }
 
 // Help returns a help message.
@@ -27,6 +39,41 @@ func (g *Group) Help() string {
 }
 
 // Parse parses the given command-line arguments, sets values for given flags and calls the function
-// for the selected command. It’s usually called with with os.Args[1:].
+// for the selected command. It’s usually called with os.Args[1:].
 func (g *Group) Run(args []string) {
+	// call Flags.parse
+	err, help, args := g.Flags.parse(args)
+	if err != nil {
+		g.fail(err.Error())
+	}
+	if help {
+		g.help()
+	}
+
+	// select group or command
+	if len(args) == 0 {
+		g.fail("command expected")
+	}
+	a := args[0]
+	args = args[1:]
+	if group, ok := g.groups[a]; ok {
+		group.Run(args)
+		return
+	}
+	if command, ok := g.commands[a]; ok {
+		_ = command
+		// ...
+	}
+	g.fail(fmt.Sprintf("'%s' is not a %s command", a, g.name))
+}
+
+func (g *Group) fail(msg string) {
+	fmt.Printf("%s: %s\n", g.name, msg)
+	fmt.Printf("Try '%s help' for more information.", g.name)
+	os.Exit(2)
+}
+
+func (g *Group) help() {
+	fmt.Print(g.Help())
+	os.Exit(0)
 }
